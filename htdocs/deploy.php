78e28ce43b1244651aeaaf64ac2031ea5b284e73
ben.dry <ben.dry@aligent.com.au>
2016-05-11 14:42:58 +0930

    adjustments

--- htdocs/deploy.php
+++ htdocs/deploy.php
@@ -6,8 +6,8 @@ function execPrint($command) {
     $result = array();
     exec($command, $result);
     foreach ($result as $line) {
-        print($line . "\n");
+        echo($line . "\n");
     }
 }
 // Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull https://captainbenno:lila2010@github.com/captainbenno/cgstarter.git") . "</pre>");
+echo("<pre>" . execPrint("git pull https://captainbenno:lila2010@github.com/captainbenno/cgstarter.git master") . "</pre>");
