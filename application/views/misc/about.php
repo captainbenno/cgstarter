f488ef59abbb0618063ad06dd9e32dbea7c72e9f
Ben Dry <ben.dry@alig1603.aligent.internal>
2016-09-08 00:21:37 +0930

    Updating Automatically

--- application/views/misc/about.php
+++ application/views/misc/about.php
@@ -7,36 +7,39 @@
             <div class="col-md-2"></div>
             <div class="col-md-8">
                 <h1>About CGStarter</h1>
+
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisi elit, eleifend eu varius rhoncus, molestie nec enim. Suspendisse tristique suscipit turpis sed commodo. 
+                    cgStarter is the new portal for Ballistic Publishing products. It is a crowd funded pre-sales site for digital art related products. It will be the exclusive outlet for all Ballistic Publishing books.
                 </p>
+
                 <p>
-                    Aenean rhoncus urna quis est varius bibendum. Donec iaculis, nisi ut ultrices gravida, lacus lorem aliquam libero, et viverra velit ipsum scelerisque dui. Morbi in pellentesque dui, vel porta urna. Maecenas molestie iaculis lacus, eget cursus sem pellentesque ut. Nulla orci massa, feugiat ut quam at, tempus posuere lacus. Aliquam sagittis faucibus velit, id auctor risus sollicitudin et. 
-                </p>
-                <p>
-                    Vestibulum faucibus dui quis risus lobortis auctor. Integer et sem in mauris posuere tristique vel a nulla. Morbi venenatis ornare odio, eu gravida risus varius in. Nam viverra laoreet convallis. Maecenas in mauris nec ante egestas ullamcorper vitae ut lorem. Pellentesque in augue bibendum, pulvinar felis sit amet, volutpat ante. Duis elementum varius ante quis tempor. Nulla pretium diam a orci dapibus, eget ullamcorper ligula convallis.
+                    cgStarter is also the place to bring your own digital art products to market. Accepted projects get access to the full expertise of the Ballistic Publishing team and global publishing system. This frees artists and editors to concentrate on collecting great content while Ballistic Publishing looks after book (or other format) creation, printing, packaging, global sales and distribution.
                 </p>
                 <h3>FAQ</h3>
                 <ul>
                     <li>
                         <h4>How can I list my project on CGStarter?</h4>
-                        <p>Over the next few weeks we will be opening up CGStarter to the public to start listing their own projects. At the moment we have allowed a small focused group of project owners to list their projects so that we can make sure we deliver the sort of experience to both listers and pledgers you would expect. If you are serious about listing a project please send an email to our team at <a href="team@cgstarter.com">team@cgstarter.com</a></p>
+                        <p>We are taking submissions for projects to be listed on CGStarter We have a schedule of A list projects that will be released on the Ballistic Publishing imprint on cgStarter. If you have a quality project you feel lives up to the Ballistic standard then please <a href="mailto:team@cgstarter.com">contact us</a></p>
                     </li>
                     <li>
                         <h4>Who runs CGStarter?</h4>
-                        <p>The same team who has been at the forefront of the CG world for over 16 years Vestibulum faucibus dui quis risus lobortis auctor. Integer et sem in mauris posuere tristique vel a nulla. Morbi venenatis ornare odio, eu gravida risus varius in. Nam viverra laoreet convallis.</a></p>
+                        <p>The same visionaries and team that created the cgSociety and Ballistic Publishing 16 years ago. The cgSociety and Ballistic Publishing were divisions of Ballistic Media. In 2015 we sold the cgSociety division to The Art Society and have since refocused our plans to re-launch Ballistic Publishing in the manner we started – with exclusive pre-sales by campaign.</p>
                     </li>
                     <li>
-                        <h4>Who runs CGStarter?</h4>
-                        <p>The same team who has been at the forefront of the CG world for over 16 years Vestibulum faucibus dui quis risus lobortis auctor. Integer et sem in mauris posuere tristique vel a nulla. Morbi venenatis ornare odio, eu gravida risus varius in. Nam viverra laoreet convallis.</a></p>
+                        <h4>How is cgStarter better than crowd funding sites?</h4>
+                        <p>cgStarter only releases A list products made and distributed to Ballistic Publishing standards. The campaigns are strictly pre-sales of know quality products without risk. Campaigns are run with number of units as targets – if a target is not met then participants receive a full refund. Once a target is met A list quality product is guaranteed to be delivered within the production and delivery times listed.</p>
                     </li>
                     <li>
-                        <h4>Who runs CGStarter?</h4>
-                        <p>The same team who has been at the forefront of the CG world for over 16 years Vestibulum faucibus dui quis risus lobortis auctor. Integer et sem in mauris posuere tristique vel a nulla. Morbi venenatis ornare odio, eu gravida risus varius in. Nam viverra laoreet convallis.</a></p>
+                        <h4>Does cgStarter work with CGSociety?</h4>
+                        <p>Yes. Ballistic Publishing and the CGSociety work closely together in a true synergy. The CGPortfolio system is used to handle all public submissions – CGportfolios and submissions are free.</p>
                     </li>
                     <li>
-                        <h4>Who runs CGStarter?</h4>
-                        <p>The same team who has been at the forefront of the CG world for over 16 years Vestibulum faucibus dui quis risus lobortis auctor. Integer et sem in mauris posuere tristique vel a nulla. Morbi venenatis ornare odio, eu gravida risus varius in. Nam viverra laoreet convallis.</a></p>
+                        <h4>Will I be able to get Ballistic books in bookstores after the campaign?</h4>
+                        <p>No. Ballistic Publishing will no longer print trade editions to be sold through resellers. You have to purchase products during a campaign or you will miss out. However there will always be bulk lots of product available in the campaign. Once the campaign closes any product available is essentially a collectable and may only be obtainable in very limited quantity, if at all, at a much higher price than originally offered in the campaign.</p>
+                    </li>
+                    <li>
+                        <h4>Will d’artiste, ESSENCE and other famous Ballistic book series continue?</h4>
+                        <p>Yes. Expect to see these series continue. Like EXPOSE they will come back in enhanced and expanded product line-ups.</p>
                     </li>
                 </ul>
             </div>
