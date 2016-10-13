d574cb8e22ed8bdf48d53b70ddfb5405348e1237
Ben Dry <ben.dry@alig1603.aligent.internal>
2016-10-13 23:07:22 +1030

    Updating Automatically

--- application/views/errors/html/error_php.php
+++ application/views/errors/html/error_php.php
@@ -1,33 +1,206 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
-<h4>A PHP Error was encountered</h4>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="shortcut icon" type="image/png" href="/favicon-16x16.png">
+	<title>CGStarter.com - DB gone bye bye</title>
 
-<p>Severity: <?php echo $severity; ?></p>
-<p>Message:  <?php echo $message; ?></p>
-<p>Filename: <?php echo $filepath; ?></p>
-<p>Line Number: <?php echo $line; ?></p>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
+		ga('create', 'UA-42678219-2', 'auto');
+		ga('send', 'pageview');
 
-	<p>Backtrace:</p>
-	<?php foreach (debug_backtrace() as $error): ?>
+	</script>
 
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
+	<meta name="keywords" content="these, are, keywords">
+	<meta name="description" content="This is the site description.">
 
-			<p style="margin-left:10px">
-			File: <?php echo $error['file'] ?><br />
-			Line: <?php echo $error['line'] ?><br />
-			Function: <?php echo $error['function'] ?>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/3.0.3/normalize.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css?v=3.2.4">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css?v=3.2.4">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css?v=3.2.4">
+	<link rel="stylesheet" href="/themes/core/css/core.css?v=3.2.4">
+	<link rel="stylesheet" href="/themes/default/css/default.css?v=3.2.4">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<! -- other -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css?v=3.2.4">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css?v=3.2.4">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css?v=3.2.4">
+	<link rel="stylesheet" href="/themes/core/css/core.css?v=3.2.4">
+	<link rel="stylesheet" href="/themes/default/css/default.css?v=3.2.4">
+	<link rel="stylesheet" href="https://cgstarter.com/themes/default/css/default.css?v=3.2.4">
+
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js?v=3.2.4"></script>
+
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js?v=3.2.4"></script>
+
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6&appId=242282062472425";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- beginning of nav -->
+<nav id="top-nav" class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="https://cgstarter.com/admin">Admin</a>
+				</li>
+				<li>
+					<a href="/logout">Logout</a>
+				</li>
+				<li class=""><a href="https://cgstarter.com/profile">Profile</a></li>
+
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="">
+					<a class="mini-cart" role="buton" href="https://cgstarter.com/cart">
+						<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+						$65</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<nav id="main-nav" class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div id="navbar-main">
+			<a class="navbar-brand" href="/"><span>CG-Starter</span></a>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="active"><a href="/">Home</a></li>
+				<li class=""><a href="/about">About</a></li>
+				<li class=""><a href="/project/expose12">Projects</a></li>
+				<li class=""><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="title-spacer-container">
+</div>
+
+
+<!-- beginning of hero -->
+<div id="hero" class="container-fluid">
+	<div id="main-logo">
+		<div id="logo-placement">
+		</div>
+	</div>
+</div>
+<!-- end of hero -->
+
+<!-- beginning of content -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8" style="padding:40px;text-align: center">
+			<h1 style="font-size: 200px">????</h1>
+			<p class="lead">
+				Strange things are afoot, chill for a bit n come on back l8r.
 			</p>
 
-		<?php endif ?>
-
-	<?php endforeach ?>
-
-<?php endif ?>
-
+		</div>
+		<div class="col-md-2"></div>
+	</div>
 </div>
+<!-- end of content -->
+<footer>
+	<div class="container-fluid">
+		<div class="row" id="footer-1">
+			<div class="col-md-2"></div>
+			<div class="col-md-2">
+				<img src="/themes/default/img/cgstarter-logo-small.png" />
+				<p>Crowd funded CG projects for all of humanity and beyond.</p>
+			</div>
+			<!--div class="col-md-2" id="tweets-feed">
+                <h4>Recents Tweets</h4>
+                <a class="twitter-timeline" href="https://twitter.com/damirdokic" data-widget-id="737237435642347520">Tweets by @damirdokic</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div -->
+			<div class="col-md-4" id="facebook-feed">
+				<h4>Facebook Feed</h4>
+				<div class="fb-page"
+					 data-href="https://www.facebook.com/ballisticpublishing/"
+					 data-tabs="timeline"
+					 data-small-header="false"
+					 data-adapt-container-width="true"
+					 data-hide-cover="true"
+					 data-show-facepile="true"
+					 data-height="300"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/ballisticpublishing/"><a href="https://www.facebook.com/ballisticpublishing/">Ballistic Publishing</a></blockquote></div></div>
+			</div>
+			<div class="col-md-2" id="footer-menu">
+				<h4>Menu</h4>
+				<ul>
+					<li>
+						<a href="/about">About and FAQ</a>
+					</li>
+					<li>
+						<a href="/privacy">Privacy Policy</a>
+					</li>
+					<li>
+						<a href="/terms">Terms and Conditions</a>
+					</li>
+					<li>
+						<a href="/shipping">Shipping and Returns</a>
+					</li>
+					<li>
+						<a href="/contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row" id="footer-2">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<img src="/themes/default/img/cgsociety-power-logo.png" />
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+</footer>
+<!-- end of footer -->
+
+
+<script type="text/javascript">
+	/**
+	 * Default theme functions
+	 */
+	$(document).ready(function() {
+
+	});
+
+</script>
+
+</body>
+</html>
