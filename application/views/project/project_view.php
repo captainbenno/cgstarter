253526f192ac55e42bf83db3f274b68c74712992
Ben Dry <ben.dry@alig1603.aligent.internal>
2016-12-09 08:04:34 +1030

    a bit of formatting

--- application/views/project/project_view.php
+++ application/views/project/project_view.php
@@ -29,24 +29,24 @@ $this->load->helper('date');
             <div id="project-tabs">
                <div class="tab-content">
                     <div role="tabpanel" class="tab-pane active" id="what">
-EXPOSÉ 12 reboot.
-You spoke and we listened.
+<h2>EXPOSÉ 12 reboot.</h2>
+<blockquote>You spoke and we listened.</blockquote>
 
-A lot of people told us they love us but they won’t order until they know what’s in the book.
+<p>A lot of people told us they love us but they won’t order until they know what’s in the book.
 It also took a lot longer to categorize the images and complete the judging.
-We have started the book layout and by late December we will have page layouts to show everyone.
+We have started the book layout and by late December we will have page layouts to show everyone.</p>
 
-So, we are going to close the EXPOSÉ 12 campaign and then reopen it late in December…
+                        <p>So, we are going to close the EXPOSÉ 12 campaign and then reopen it late in December…</p>
 
-We have offered all the early backers a special offer for their support and having to wait even longer.
+                        <p>We have offered all the early backers a special offer for their support and having to wait even longer.</p>
 
-What we can tell you is that we received about 7,000 fantastic entries which have been categorized and judged. There is just as much interest in EXPOSÉ as ever. One change we are implementing is an expansion of the categories which will mean we can recognize and honour more winners. We will also be doing at least one eBook edition so we can feature more of the awesome artists and the work submitted… EXPOSÉ 12 is going to be fantastic!
-We had many unplanned delays due to a complete re-write of our entry and judging systems, but our layout, printing and packaging are all going to be the same as our previous books – the best in the world.
+                        <p>What we can tell you is that we received about 7,000 fantastic entries which have been categorized and judged. There is just as much interest in EXPOSÉ as ever. One change we are implementing is an expansion of the categories which will mean we can recognize and honour more winners. We will also be doing at least one eBook edition so we can feature more of the awesome artists and the work submitted… EXPOSÉ 12 is going to be fantastic!
+We had many unplanned delays due to a complete re-write of our entry and judging systems, but our layout, printing and packaging are all going to be the same as our previous books – the best in the world.</p>
 
 
-From the whole Ballistic Publishing team.
-Mark Snoswell,
-Founder and Director Ballistic Media.
+                        <p>From the whole Ballistic Publishing team.<br>
+Mark Snoswell,<br>
+Founder and Director Ballistic Media.</p>
                     </div>
                </div>
     </div>
