1c9ea26fdb268fa31102d794dcfa2929499e27df
root <root@ip-172-31-48-133.ec2.internal>
2017-02-15 10:35:57 +0000

    adding stuffwq

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cgstarter.com/';
+$config['base_url'] = 'https://cgstarter.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -385,7 +385,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
+$config['cookie_secure']	= TRUE;
 $config['cookie_httponly'] 	= FALSE;
 
 /*
